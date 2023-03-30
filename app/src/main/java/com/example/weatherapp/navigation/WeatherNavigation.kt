a41d095b4809ceb910eb5ddd2780e95da72e1650
Venkat Gonuguntala <venkyg88@gmail.com>
2023-03-30 17:29:52 -0400

    Add splash screen and initial main screen

---
+++ app/src/main/java/com/example/weatherapp/navigation/WeatherNavigation.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.screens.WeatherMainScreen
+import com.example.weatherapp.screens.WeatherSplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = WeatherScreens.SplashScreen.name){
+        composable(WeatherScreens.SplashScreen.name) {
+            WeatherSplashScreen(navController = navController)
+        }
+        composable(WeatherScreens.MainScreen.name) {
+            WeatherMainScreen(navController = navController)
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/weatherapp/navigation/WeatherScreens.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.navigation
+
+enum class WeatherScreens {
+
+    SplashScreen,
+    MainScreen,
+    SearchScreen,
+    FavoriteScreen
+}
