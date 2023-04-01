84ac6975dd13b13ed826c25c281ae4669bb29970
Venkat Gonuguntala <venkyg88@gmail.com>
2023-03-31 22:31:49 -0400

    add suspend fun that fetches weather by coordinates

--- app/src/main/java/com/example/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/repository/WeatherRepository.kt
@@ -8,10 +8,10 @@ import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(private val api: WeatherApi) {
 
-    suspend fun getWeather(city: String, units: String)
+    suspend fun getWeather(city: String?)
     : DataOrException<Weather, Boolean, Exception> {
         val response = try {
-            api.getWeatherByCity(city = city, units = units)
+            api.getWeatherByCity(city = city)
 
         }catch (e: Exception){
             Log.d("REX", "getWeather: $e")
@@ -19,7 +19,22 @@ class WeatherRepository @Inject constructor(private val api: WeatherApi) {
         }
         Log.d("INSIDE", "getWeather: $response")
         return  DataOrException(data = response)
+    }
 
+    suspend fun getWeatherByCoordinates(
+        latitude: String,
+        longitude: String,
+    ): DataOrException<Weather, Boolean, Exception> {
+
+        Log.d("coordinates","${latitude} +, ${longitude}")
+        val response = try {
+            api.getWeatherByCoordinates(latitude = latitude, longitude = longitude)
+        }catch (e: Exception){
+            Log.d("Repo response by coordinates", "getWeather: $e")
+            return DataOrException(e = e)
+        }
+        Log.d("INSIDE", "getWeather: $response")
+        return  DataOrException(data = response)
     }
 
 }
