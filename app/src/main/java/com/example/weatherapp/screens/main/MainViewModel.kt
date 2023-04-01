08a2ea9be8524f1885701687aba18cfaa19e3856
Venkat Gonuguntala <venkyg88@gmail.com>
2023-03-31 22:15:25 -0400

    add support for weather retrieval by city and location

--- app/src/main/java/com/example/weatherapp/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/screens/main/MainViewModel.kt
@@ -1,38 +1,26 @@
 package com.example.weatherapp.screens.main
 
-import android.util.Log
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.data.DataOrException
 import com.example.weatherapp.model.Weather
 import com.example.weatherapp.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.Exception
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val repository: WeatherRepository):
+class MainViewModel @Inject constructor(private val repository: WeatherRepository) :
     ViewModel() {
-        val data: MutableState<DataOrException<Weather, Boolean, Exception>>
-        = mutableStateOf(DataOrException(null, true, Exception("")))
 
-    init {
-        loadWeather()
+    suspend fun getWeatherByCity(city: String): DataOrException<Weather, Boolean, Exception> {
+        return repository.getWeather(city = city)
     }
 
-    private fun loadWeather() {
-        getWeather("Cary")
+    suspend fun getWeatherByCoordinates(
+        latitude: String,
+        longitude: String,
+    ): DataOrException<Weather, Boolean, Exception> {
+        return repository.getWeatherByCoordinates(latitude, longitude)
     }
 
-    private fun getWeather(city: String) {
-        viewModelScope.launch {
-            if(city.isEmpty()) return@launch
-            data.value.loading = true
-            data.value = repository.getWeather(city = city, "imperial")
-            if(data.value.data.toString().isNotEmpty()) data.value.loading = false
-        }
-        Log.d("GET","getWeather: ${data.value.data.toString()}")
-    }
 }
